d730a5caaf3e0a9b59cc49170d3c8a8e0d144c47
David Coomber <47242934+dcoomber@users.noreply.github.com>
2021-11-27 07:31:29 +0200

    Added README, first usable version of script

--- scripts/assembla_migrate_1_extract_tickets.rb
+++ scripts/assembla_migrate_1_extract_tickets.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/env ruby
-# Reference https://www.twilio.com/blog/5-ways-make-http-requests-ruby
+# References
+# -- https://www.twilio.com/blog/5-ways-make-http-requests-ruby
+# -- https://www.rubyguides.com/2019/01/ruby-environment-variables/
 
 require 'uri'
 require 'net/http'
 require 'json'
 
-base_uri = 'http://hotel-test.equalexperts.io'
+assembla_api_key = ENV['ASSEMBLA_API_KEY']
+assembla_api_secret = ENV['ASSEMBLA_API_SECRET']
+assembla_base_uri = 'http://hotel-test.equalexperts.io'
+header = { 'X-Api-Key': assembla_api_key, 'X-Api-Secret': assembla_api_secret }
 
 def clean_up(file)
   file.close
@@ -15,13 +20,12 @@ end
 # 240 requests per minute with burst of 20 requests per second
 
 # Get the list of Assembla spaces
-uri = URI("#{base_uri}/booking")
-spaces_json = Net::HTTP.get_response(uri)
+uri = URI("#{assembla_base_uri}/booking")
+spaces_json = Net::HTTP.get_response(uri, header)
 exit unless spaces_json.is_a?(Net::HTTPSuccess)
 
 # https://api-docs.assembla.cc/content/ref/spaces_index.html
 puts 'GET /v1/spaces (200 OK, 204 No Content, 404 Not Found)'
-
 
 spaces_json = <<'JSON_STRING'
   [
@@ -78,8 +82,8 @@ end
 puts "\nQuery the tickets in space: #{spaces[answer - 1]['name']}"
 output_file_name = "#{spaces[answer - 1]['name']}.csv"
 
-uri = URI("#{base_uri}/booking")
-tickets_json = Net::HTTP.get_response(uri)
+uri = URI("#{assembla_base_uri}/booking")
+tickets_json = Net::HTTP.get_response(uri, header)
 exit unless tickets_json.is_a?(Net::HTTPSuccess)
 
 # https://api-docs.assembla.cc/content/ref/tickets_index.html
@@ -124,10 +128,8 @@ tickets = JSON.parse tickets_json
 
 # Open the output file
 output_file = File.open(output_file_name, 'w')
-
-output_file.write("number,title,description\n")
-
 expected_number = 1
+output_file_text = "number,title,description,status\n"
 tickets.each_with_index do |ticket, i|
   if ticket['number'].to_i != expected_number
     puts "Tickets are not sequential.  Check index #{i}: expected #{expected_number}, got #{ticket['number'].to_i}"
@@ -135,9 +137,12 @@ tickets.each_with_index do |ticket, i|
     exit
   end
   # Extract ID, Title, Description (not sure about description - just create a hyperlink to the Assembla issue)
-  output_file.write("#{ticket['number']},\"#{ticket['summary']}\",\"#{ticket['description']}\"\n")
+  output_file_text << "#{ticket['number']},\"#{ticket['summary']}\","
+  output_file_text << "\"#{ticket['description']}\",\"#{ticket['status_name']}\"\n"
+  
   expected_number += 1
 end
 
+output_file.write(output_file_text)
 clean_up(output_file)
 puts 'Processing completed!'
