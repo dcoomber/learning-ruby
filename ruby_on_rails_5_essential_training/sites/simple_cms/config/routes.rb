6b89795fe3d0620728a319a770ad14f9c57d5b33
David Coomber <47242934+dcoomber@users.noreply.github.com>
2021-12-11 14:56:40 +0200

    Finished controllers and views

--- ruby_on_rails_5_essential_training/sites/simple_cms/config/routes.rb
+++ ruby_on_rails_5_essential_training/sites/simple_cms/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  get 'example/view'
-  root 'demo#index'
-  get 'demo/index'
   # default route
   # may be removed in future versions
   # get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'example/view'
+  root 'demo#index'
+  get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
 end
