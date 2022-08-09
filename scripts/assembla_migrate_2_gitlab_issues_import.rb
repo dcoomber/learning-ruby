b54129529919e060906e60f8383aecd312fdbe0b
David Coomber <47242934+dcoomber@users.noreply.github.com>
2022-08-09 14:13:24 +0200

    Clean-up files

--- scripts/assembla_migrate_2_gitlab_issues_import.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-gitlab_personal_access_token = ENV['GITLAB_PERSONAL_ACCESS_TOKEN']
-gitlab_base_uri = 'http://hotel-test.equalexperts.io'
-
-uri = URI("#{gitlab_base_uri}/booking")
-header = { 'PRIVATE-TOKEN': gitlab_personal_access_token }
-projects_json = Net::HTTP.get_response(uri, header)
-exit unless projects_json.is_a?(Net::HTTPSuccess)
